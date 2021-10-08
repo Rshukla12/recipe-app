7670eeee3803549969c0e0102ba0bb5d920faaa1
Ravi Shukla <rshuklabbb@gmail.com>
2021-10-08 20:26:44 +0530

    Added fetching and displaying of random meals in the today's special page

--- scripts/special.js
+++ scripts/special.js
@@ -1,5 +1,40 @@
 import navbar from "../components/navbar.js";
+import createDishCard from "../components/dishCard.js";
+
+
+function fetchRandom(){
+    return fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+            .then( ( res ) => res.json() )
+}
+
+function displayDetails(dish){
+    const cont = document.getElementById("container");
+    cont.innerHTML = null;
+
+}
+
+function addBtn(target, dish){
+    const btn = document.createElement("button");
+    btn.textContent = "Display Details";
+    btn.addEventListener("click", displayDetails(dish));
+    target.append(btn)
+}
+
+
+async function handleLoad(){
+    try {
+        const cont = document.getElementById("container");
+        const {meals} = await fetchRandom();
+        const res = createDishCard( meals[0] );
+        addBtn(res, meals[0]);
+        cont.append( res );
+    } catch ( err ){
+        console.log(err);
+    }
+}
+
 
 window.addEventListener( "load", () => {
     document.querySelector("nav").innerHTML = navbar( { title: "Recipe of The Day", id:1 } );
+    handleLoad();
 })
