d319caed14a0f1e56f16c38db12a76fbd498a00a
Ravi Shukla <rshuklabbb@gmail.com>
2021-10-10 00:06:27 +0530

    Added show details to display recipe in detail in todays special recipe

--- scripts/special.js
+++ scripts/special.js
@@ -7,16 +7,12 @@ function fetchRandom(){
             .then( ( res ) => res.json() )
 }
 
-function displayDetails(dish){
-    const cont = document.getElementById("container");
-    cont.innerHTML = null;
-
-}
-
 function addBtn(target, dish){
     const btn = document.createElement("button");
     btn.textContent = "Display Details";
-    btn.addEventListener("click", displayDetails(dish));
+    btn.addEventListener("click", ()=>{
+        displayDetails(dish)
+    });
     target.append(btn)
 }
 
@@ -33,6 +29,40 @@ async function handleLoad(){
     }
 }
 
+
+function displayDetails(dish){
+    const target = document.getElementById("container");
+    const cont = document.createElement("div");
+    const img = document.createElement("img");
+    const info = document.createElement("div");
+    const name = document.createElement("h1");
+    const cate = document.createElement("h3");
+    const cout = document.createElement("h3");
+    const table = document.createElement("ul");
+    const inst = document.createElement("p");
+
+    target.className = "new-cont";
+
+    name.textContent = dish.strMeal;
+    img.src = dish.strMealThumb;
+    cate.textContent = dish.strCategory;
+    cout.textContent = dish.strArea;
+    inst.textContent = dish.strInstructions;
+
+    // create table
+    for ( let i = 1; i <= 20; i++ ){
+        const li = document.createElement("li");
+        if ( dish[`strIngredient${i}`] == "" ){
+            break;
+        }
+        li.textContent = dish[`strIngredient${i}`] + " - " + dish[`strMeasure${i}`]
+        table.append(li)
+    }
+    info.append( name, cate, cout, inst, table );
+    cont.append( img, info );
+    target.innerHTML = null;
+    target.append( cont );
+}
 
 window.addEventListener( "load", () => {
     document.querySelector("nav").innerHTML = navbar( { title: "Recipe of The Day", id:1 } );
