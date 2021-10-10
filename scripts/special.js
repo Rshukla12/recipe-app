8aa940a686f15880cbf1256704114e6860930151
Ravi Shukla <rshuklabbb@gmail.com>
2021-10-10 15:36:58 +0530

    Added styling for todays special page

--- scripts/special.js
+++ scripts/special.js
@@ -29,37 +29,59 @@ async function handleLoad(){
     }
 }
 
+function createRow(val){
+
+}
+
 
 function displayDetails(dish){
     const target = document.getElementById("container");
     const cont = document.createElement("div");
+    const cont2 = document.createElement("div");
     const img = document.createElement("img");
     const info = document.createElement("div");
     const name = document.createElement("h1");
     const cate = document.createElement("h3");
     const cout = document.createElement("h3");
-    const table = document.createElement("ul");
+    const table = document.createElement("table");
     const inst = document.createElement("p");
 
     target.className = "new-cont";
+    table.border = "1"
 
     name.textContent = dish.strMeal;
     img.src = dish.strMealThumb;
-    cate.textContent = dish.strCategory;
-    cout.textContent = dish.strArea;
-    inst.textContent = dish.strInstructions;
+    cate.textContent = "Category - " + dish.strCategory;
+    cout.textContent = "Region - " + dish.strArea;
+    inst.innerText = dish.strInstructions;
+
+    const t = document.createElement("tr");
+    const th = document.createElement("th");
+    const th1 = document.createElement("th");
+
+    th.textContent = "Ingredient";
+    th1.textContent = "Measure";
+
+    t.append(th, th1);
+    table.append(t);
 
     // create table
     for ( let i = 1; i <= 20; i++ ){
+        const tr = document.createElement("tr");
         const li = document.createElement("li");
-        if ( dish[`strIngredient${i}`] == "" ){
+        if ( !dish[`strIngredient${i}`] ){
             break;
         }
-        li.textContent = dish[`strIngredient${i}`] + " - " + dish[`strMeasure${i}`]
-        table.append(li)
+        const td1 = document.createElement("td");
+        const td2 = document.createElement("td");
+        td1.textContent = dish[`strIngredient${i}`];
+        td2.textContent = dish[`strMeasure${i}`];
+        tr.append(td1, td2)
+        table.append(tr);
     }
-    info.append( name, cate, cout, inst, table );
-    cont.append( img, info );
+    cont2.append( img, cate, cout )
+    info.append( name, inst, table );
+    cont.append( cont2, info );
     target.innerHTML = null;
     target.append( cont );
 }
